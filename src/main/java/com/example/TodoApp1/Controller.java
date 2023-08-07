8c989c0bcfb008da9b0dcf2c90f17c907cf40e80
이호연 <hyl1546@gmail.com>
2023-08-07 23:54:00 +0900

    TodoApplication 만들기...

---
+++ src/main/java/com/example/TodoApp1/Controller.java
@@ -0,0 +1,13 @@
+package com.example.TodoApp1;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
